994ad5f7928713e19bf44bbdce0a0ac390bf9eda
Ben Roper <benroper42@gmail.com>
2024-03-08 15:53:25 -0700

    the whole darn thing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import teamData from "./CollegeBasketballTeams.json";
+
+interface TeamInfo {
+  tid: number;
+  cid: number;
+  did: number;
+  school: string;
+  name: string;
+  abbrev: string;
+  pop: number;
+  city: string;
+  state: string;
+  latitude: number;
+  longitude: number;
+}
+
+function Welcome() {
+  return <h1 style={{ textAlign: "center" }}>NCAA College Basketball Teams</h1>;
+}
+
+class Team extends React.Component<TeamInfo> {
+  render() {
+    const oneTeam = this.props;
+
+    return (
+      <div
+        style={{
+          width: "calc(25% - 20px)",
+          margin: "10px",
+          padding: "15px",
+          border: "1px solid #ccc",
+          borderRadius: "5px",
+          boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+        }}
+      >
+        <h2>{oneTeam.school}</h2>
+        <h3>Mascot: {oneTeam.name}</h3>
+        <h3>
+          Location: {oneTeam.city}, {oneTeam.state}
+        </h3>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <>
+        <Welcome />
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "space-around",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {teamData.teams.map((team) => (
+            <Team key={team.tid} {...team} />
+          ))}
+        </div>
+      </>
     </div>
   );
 }
